c114c17fb9fbd5c9f5d09a2b661dce89db97a6d3
GiordanoDiego <diegomgiordano96@gmail.com>
2024-02-07 14:58:39 +0100

    mileston 1 completed

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<?php
+    function getName($n) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ?!-*$&';
+        $randomString = '';
+     
+        for ($i = 0; $i < $n; $i++) {
+            $index = rand(0, strlen($characters) - 1);
+            $randomString .= $characters[$index];
+        }
+     
+        return $randomString;
+    };
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -9,15 +22,17 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         <link rel="stylesheet" href="./css/style.css">
         <!-- TITLE PAGE -->
-        <title>Document</title>
+        <title>Password Generator</title>
     </head>
 
         <!-- START BODY -->
     <body class="debug">
         <!-- START MAIN HEADER -->
         <header>
-            <div class="container">
-                HEAD
+            <div class="container text-center ">
+                <h1>
+                    Password Generator
+                </h1>
             </div>
         </header>
         <!-- END MAIN HEADER -->
@@ -25,18 +40,47 @@
         <!-- START MAIN -->
         <main>
             <div class="container">
-                MAIN
+                <div class="row">
+                    <div class="col-12">
+                        <form action="" method="$_GET">
+                            <label for="lengthPSW">Inserisci lunghezza password da generare: </label>
+                            <input type="number" id="lengthPSW" min="8" max="20" name="lengthPSW">
+                            <button class=" ms-2 btn btn-success">Genera!</button>
+                        </form>
+                        <hr>
+                    </div>
+                    <div class="col-12">
+                        <p>
+                            Lunghezza password scelta: 
+                            <?php 
+                                if(isset($_GET["lengthPSW"]) && !empty($_GET["lengthPSW"])){ //controllo se l'array get[lengthPSW] esiste o se è vuoto
+                                    echo $_GET["lengthPSW"];
+                                }    
+                            ?>
+                        </p>
+                        <hr>
+                        <div>
+                            <h6>
+                                La password di lunghezza 
+                                <strong>
+                                    <?php 
+                                        if(isset($_GET["lengthPSW"]) && !empty($_GET["lengthPSW"])){
+                                            echo $_GET["lengthPSW"]; 
+                                        }?>
+                                </strong> 
+                                generata casualmente è:
+                                <?php 
+                                    if(isset($_GET["lengthPSW"]) && !empty($_GET["lengthPSW"])){
+                                        echo getName($_GET["lengthPSW"]);
+                                    }?> 
+                            </h6>
+                        </div>
+                    </div>
+                </div>
             </div>
         </main>
         <!-- END MAIN -->
 
-        <!-- START FOOTER -->
-        <footer>
-            <div class="container">
-                FOOTER
-            </div>
-        </footer>
-        <!-- END FOOTER -->
         <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
         <script src="./js/script.js"></script>
