e9520fb68ce588798f5d6b967b06764f96f77e66
GiordanoDiego <diegomgiordano96@gmail.com>
2024-02-07 15:09:09 +0100

    completed

--- index.php
+++ index.php
@@ -1,15 +1,5 @@
 <?php
-    function getName($n) {
-        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ?!-*$&';
-        $randomString = '';
-     
-        for ($i = 0; $i < $n; $i++) {
-            $index = rand(0, strlen($characters) - 1);
-            $randomString .= $characters[$index];
-        }
-     
-        return $randomString;
-    };
+    include __DIR__.'/functions.php'; 
 ?>
 <!DOCTYPE html>
 <html lang="en">
